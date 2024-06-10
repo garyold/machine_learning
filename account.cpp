48542521cbe39d31432da1c27f8edce4054cff9f
garyold <69456709+garyold@users.noreply.github.com>
2024-06-10 12:16:01 +0800

    Add files via upload
    main operation

---
+++ account.cpp
@@ -0,0 +1,37 @@
+#include "common.h"
+#include "account_item.h"
+
+int main()
+{
+    vector<AccountItem> items;
+    loadDataFromFile(items);
+
+    bool quit = false;
+    while(!quit) 
+    {
+        showMainMenu();
+
+        char key = readMenuSelection(3);
+
+        switch(key)
+        {
+          case '1': 
+              showAccountingMenu();
+              accounting(items);
+              break;
+          case '2': 
+              showQueryMenu();
+              query(items);
+              break;
+          case '3': 
+              cout << "\n確認退出? (Y/N): ";
+              if(readQuitConfirm() == 'Y')
+                  quit = true;
+              break;
+          default: 
+              break;
+        }
+
+        cout << endl;
+    }
+}
